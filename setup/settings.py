77305a9241074528fc85355974f27b386a1f0e6f
andrlukk <lucasandr17@outlook.com>
2025-10-01 12:15:58 -0300

    att

--- setup/settings.py
+++ setup/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = "true"
+DEBUG = os.getenv("DEBUG_VARIABLE").lower() == "true"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(" ")
 
